f128c2c91c35e7ccfae75c4da29e96a1f5405814
Connor Milligan <connoramilligan@gmail.com>
2023-12-18 19:40:14 -0500

    added some colors to term output

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include "base.h"
 
 #include <stdio.h>
-int main() {
+
+int main(int argc, char **argv) {
+
+    // Print the values of the macros
+    printf("Compiler Definitions:\n");
     printf("cl    = %d\n", COMPILER_CL);
     printf("clang = %d\n", COMPILER_CLANG);
     printf("gcc   = %d\n", COMPILER_GCC);
@@ -13,5 +17,14 @@ int main() {
     printf("arm   = %d\n", ARCH_ARM);
     printf("arm64 = %d\n", ARCH_ARM64);
 
-    return(0);
+    // Color test
+    printf("\nColor test:\n");
+    printf("This is %sred%s.\n", TERM_COLOR_RED, TERM_COLOR_RESET);
+    printf("This is %sgreen%s.\n", TERM_COLOR_GREEN, TERM_COLOR_RESET);
+    printf("This is %syellow%s.\n", TERM_COLOR_YELLOW, TERM_COLOR_RESET);
+    printf("This is %sblue%s.\n", TERM_COLOR_BLUE, TERM_COLOR_RESET);
+    printf("This is %smagenta%s.\n", TERM_COLOR_MAGENTA, TERM_COLOR_RESET);
+    printf("This is %scyan%s.\n", TERM_COLOR_CYAN, TERM_COLOR_RESET);
+
+    return 0;
 }
