85a51d50e7a4bd0ae229669b92c507a1a8dcd743
Connor Milligan <connoramilligan@gmail.com>
2023-12-21 03:06:30 +0100

    fixed cmake version, made color not display on win

--- src/main.c
+++ src/main.c
@@ -15,10 +15,13 @@ int main(int argc, char **argv) {
     printf("x64   = %d\n", ARCH_X64);
     printf("x86   = %d\n", ARCH_X86);
     printf("arm   = %d\n", ARCH_ARM);
-    printf("arm64 = %d\n", ARCH_ARM64);
+    printf("arm64 = %d\n\n", ARCH_ARM64);
 
     // Color test
-    printf("\nColor test:\n");
+#if OS_WINDOWS
+    printf(" ~ This system (Windows) does not support colored output with this framework. ~ \n");
+#endif
+    printf("Color test:\n");
     printf("This is %sred%s.\n", TERM_COLOR_RED, TERM_COLOR_RESET);
     printf("This is %sgreen%s.\n", TERM_COLOR_GREEN, TERM_COLOR_RESET);
     printf("This is %syellow%s.\n", TERM_COLOR_YELLOW, TERM_COLOR_RESET);
